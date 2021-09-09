fb6abfb0dff14fc46b3f7cbfc6a8fc3d61c31b73
Man E <mannyportillo77@gmail.com>
2021-09-09 15:55:23 -0600

    created successful POST route

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const { query } = require('express');
 const express = require ('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+  // parse incoming string or array data
+  app.use(express.urlencoded({ extended: true })); //built in method; takes incoming POST data and converts it to key/value pairs that are accessed in req.body
+  // parse incoming JSON data
+  app.use(express.json()); //takes incoming POST data in the form of JSON and parses it into the req.body javascript object
+  //need both MIDDLEWARE functions set up every time you create a server that is looking to accept POST data 
 const { animals } = require( './data/animals')
 
 function filterByQuery(query, animalsArray) {
@@ -47,6 +54,18 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  // our function's main code will go here!
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results =  animals;
     if (req.query) {
@@ -58,12 +77,24 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
  
-  //return error message if no id match
+  //return error message if no id
   if (result) {
     res.json(result);
   } else {
     res.send(404); // error message, standard format
   }
+});
+
+app.post('/api/animals', (req, res) => {
+
+  //set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  //add animals to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  //req.body is where our incoming content will be
+  console.log(req.body); //use console log to view the data
+  res.json(animal); //use res.json to send the data back to the client
 });
 
 app.listen(PORT, () => {
